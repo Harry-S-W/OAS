f1f486ff15f5eff8d02344c5a427a8881478a44b
harrywoodhouse <woodhouses.harry@gmail.com>
2025-10-06 11:25:52 +0100

    Re-adding missing files

---
+++ oas/schemas.py
@@ -0,0 +1,17 @@
+GENERAL_COLS = [
+    'frame','x_outer','x_outer_uncertainty','x_inner','x_inner_uncertainty',
+    'y_outer','y_outer_uncertainty','y_inner','y_inner_uncertainty',
+    'x_anchor','x_anchor_uncertainty','y_anchor','y_anchor_uncertainty'
+]
+
+CENTRED_COLS = (
+    ["frame"]
+    + [f"x_{i}" for i in range(48, 68)]
+    + [f"y_{i}" for i in range(48, 68)]
+)
+
+CURVE_COEFF_COLS = [
+    "frame","region","degree",
+    "Ax","Bx","Cx","Dx","Ay","By","Cy","Dy",
+    "X0","Y0","X1","Y1","X2","Y2","X3","Y3",
+]
